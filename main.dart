b4c240435c02a6bfcf80a2fe2907d181010673d3
Fozia AttaurRehman <developera213@gmail.com>
2023-07-25 23:22:23 +0500

    Comments in Flutter

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+  // This is a single-line comment.
+
+  /*
+   * This is a multi-line comment.
+   * It can span across multiple lines.
+   */
+
+  /// This is a documentation comment.
+  /// It is used for generating documentation for your code.
+  /// You can include detailed descriptions of classes, methods, or variables.
+
+  // TODO: Implement this feature.
+
+  // FIXME: Fix this issue before release.
+
+  // NOTE: This part of the code is critical for performance.
+}
